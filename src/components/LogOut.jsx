83c327c9dfe3310b8fbe2a18dd6bf984b7125e04
Yavuz1997 <yavuzgogus@gmail.com>
2024-01-23 22:14:25 +0300

    Update LogOut.jsx

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
   const navigate = useNavigate();
